57836c5092324343eb03b97e2f995b46137fec14
Mike Nichols <mikenichols410@gmail.com>
2019-07-11 12:17:51 -0400

    Global updates

---
+++ Online-Project-Ideas/Udemy-Courses/udemy-react/monsters-rolodex/src/App.js
@@ -0,0 +1,55 @@
+import React  from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.constant = `this variable is set in stone`
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1>)
+        }
+      </div>
+    );
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
