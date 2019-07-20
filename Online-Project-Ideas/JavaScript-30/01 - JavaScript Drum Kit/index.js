c7a1b7ff5fd98d451106d54d6a73f73012d8cb9f
Mike Nichols <mikenichols410@gmail.com>
2019-07-20 00:57:25 -0400

    Updated drumkit, dit preprocessing 2, sprint, and worked on react website

--- Online-Project-Ideas/JavaScript-30/01 - JavaScript Drum Kit/index.js
+++ Online-Project-Ideas/JavaScript-30/01 - JavaScript Drum Kit/index.js
@@ -1,41 +1,60 @@
 (function(global){
-const keys = document.querySelectorAll('.key');
-class DrumKit {
-    makeNoise(audio,key){
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
+    const keys = document.querySelectorAll('.key');
+    class DrumKit {
+        constructor() {
+            this.memory = [];
+        }
+        makeNoise(audio,key){
+        if(!audio) return;
+        this.memory.push(audio);
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+        }
+        removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+        }
+        playBack(){
+            const audio = this.memory; 
+            let i = 0;
+                setInterval(() => {
+                    if (i < audio.length ){    
+                        audio[i].play();
+                        document.querySelector(`.key[data-key='${audio[i].dataset.key}']`).classList.add('playing'); // Adds playing class to the key being played back
+                        i++;
+                    };
+                }, 375) // Replays audio from memory every 375ms 
+                document.getElementById('replay').classList.add('playing');
+            }  
     }
-    removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+    const drumKit = new DrumKit();
+    class Sound extends DrumKit{
+        constructor(){
+            super()
+        }
+        keyNoise(keypress) {
+        if (keypress.keyCode === 13) {drumKit.playBack()};
+        const audio = document.querySelector(`audio[data-key="${keypress.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${keypress.keyCode}"`);
+        drumKit.makeNoise(audio,key)
+        }
+        clickNoise(click) {    
+        click = click.path[1].dataset.key;
+        const audio = document.querySelector(`audio[data-key="${click}"]`);
+        const key = document.querySelector(`button.key[data-key="${click}"`);
+        drumKit.makeNoise(audio,key);
     }
-}
-const drumKit = new DrumKit();
-class Sound extends DrumKit{
-    constructor(){
-        super()
     }
-    keyNoise(keypress) {
-    const audio = document.querySelector(`audio[data-key="${keypress.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${keypress.keyCode}"`);
-    drumKit.makeNoise(audio,key)
-    }
-    clickNoise(click) {    
-    click = click.path[1].dataset.key;
-    const audio = document.querySelector(`audio[data-key="${click}"]`);
-    const key = document.querySelector(`button.key[data-key="${click}"]`);
-    drumKit.makeNoise(audio,key);
-}
-}
-const sound = new Sound();
-global.addEventListener('keydown', sound.keyNoise);
-keys.forEach(function(key) {
-    key.addEventListener('click',sound.clickNoise);
-    key.addEventListener('contextmenu',function(e){
-      e.preventDefault();
+    const sound = new Sound()
+    
+    global.addEventListener('keydown', sound.keyNoise);
+    keys.forEach(function(key) {
+        key.addEventListener('click',sound.clickNoise);
+        key.addEventListener('contextmenu',function(e){
+          e.preventDefault();
+        });
+        key.addEventListener('transitionend',drumKit.removeTransition);
     });
-    key.addEventListener('transitionend',drumKit.removeTransition);
-});
-})(window)
+    document.querySelector('#replay').addEventListener('click', sound.playBack)
+    })(window)
